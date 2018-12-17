201c323158719ac96c2f9457e1c7096df6752cc7
li970140759 <1620893891@qq.com>
2018-12-17 20:47:21 +0800

    user部分完成

---
+++ public/MyPc/js/user.js
@@ -0,0 +1,57 @@
+$(function () {
+    var currentPage = 1
+    var pageSize = 5
+    var currentId
+    var isDelete
+    render()
+    function render() {
+        $.ajax({
+            type: 'get',
+            url: '/user/queryUser',
+            data: {
+                page: currentPage,
+                pageSize: pageSize
+            },
+            dataType: 'json',
+            success: function (info) {
+                console.log(info)
+                var htmlStr = template('tpl', info)
+                $('tbody').html(htmlStr)
+
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: info.page,
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, page) {
+                        currentPage = page;
+                        render();
+                    }
+                })
+            }
+        })
+    }
+
+    $('tbody').on('click', '.btn', function () {
+        $('#userModal').modal('show')
+        currentId = $(this).parent().data('id')
+        isDelete = $(this).hasClass('btn-danger') ? 0 : 1
+    })
+    $('#submitBtn').click(function () {
+        $.ajax({
+            type: 'post',
+            url: '/user/updateUser',
+            dataType: 'json',
+            data: {
+                id: currentId,
+                isDelete: isDelete
+            },
+            success: function (info) {
+                if (info.success) {
+                    $('#userModal').modal('hide')
+                    render()
+                }
+            }
+        })
+
+    })
+})
